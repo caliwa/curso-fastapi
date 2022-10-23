f6eb23d0bca388d336339ef42dacb8c22a8f5d1a
Carlos Gonzalez <carlos_gonzalez82201@elpoli.edu.co>
2022-10-22 19:57:44 -0500

    Añandiendo Procfile

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(auth.router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port = 8000, reload=True) #ahora solo se ejecuta el archivo
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", port = 8000, reload=True) #ahora solo se ejecuta el archivo
